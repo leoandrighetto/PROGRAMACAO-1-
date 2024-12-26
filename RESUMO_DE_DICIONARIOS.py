650082ddf646078f748cbcf9df0dd5ae89c80a63
leoandrighetto <leoandrighetto02@gmail.com>
2024-12-26 20:22:51 -0300

    RESUMO_DE_DICIONARIOS.py

--- RESUMO_DE_DICIONARIOS.py
+++ RESUMO_DE_DICIONARIOS.py
@@ -1,12 +1,25 @@
 #FORMAS DE DECLARAR DICIONÁRIOS:
                 #111111111
+def Linha():
+    print()
+    print(19 *  '--------')
+    print(19 *  '--------')
+    print()
+
+Linha()
 
 leonardo={
     "Nome": "Leonardo Andrighetto Linhares" ,
     "Telefone": "555-(51) 99387-7613",
     "Endereço": "Acesso Bonar Figueiro 2537 Restinga Porto Alegre"}
-print(f'Dicionario1 (Leonardo):{leonardo}')
-print('------------')
+
+print(f'Dicionario1 (Leonardo):')
+
+for i in leonardo.items():
+    print(i)
+  
+
+Linha()
 
                 #222222222
 leonardo2 = {}
@@ -14,87 +27,103 @@ leonardo2["Nome"]="Leonardo Andrighetto Linhares"
 leonardo2["Telefone"]="993877613"
 leonardo2["Endereço"]="Rua Tal"
 
-print(f'Dicionario2 (Leonardo2): {leonardo2}')
-print('------------')
+print(f'Dicionario2 (Leonardo2)')
 
+for i in leonardo2.items():
+    print(i)
+
+Linha()
 
 
 #CONSULTAS USANDO " .get()  "
 
 #
-print(f'Comando -> "leonardo.get("Nome")" -> : {leonardo.get("Nome")}')
+print(f'Comando -> "leonardo.get("Nome")" RETORNO:')
+print(leonardo.get("Nome"))
 #
-print('------------')
+
+Linha()
+
 
 #PARA ADICIONAR OU ALTERAR CHAVES USANDO    "  update()  "
 
 #               ---
-print(f'Nome antes de Alterar com update: {leonardo2.get("Nome")}')
+print(f'Nome antes de Alterar com update:')
+print(leonardo2.get("Nome"))
 #    (dicionario.get('chave'))
-print('------------')
 
+Linha()
 
 leonardo.update({"Nome": "Leleo"})
 
-print(f'Depois de mudar Nome usando Update (Leonardo.update) -> : {leonardo.get("Nome")}')
-print('------------')
+print(f'Depois de mudar Nome usando Update:')
+print(leonardo.get("Nome"))
+
+Linha()
                                 #adicionando uma nova chave:
 leonardo.update({"Nome da Mãe": 'Ivete Andrighetto'})
 
-print(f'Após nova chave: {leonardo}')
-print('------------')
+print(f'Após nova chave:')
+for i in leonardo.items():
+
+    print(i)
+
+Linha()
 
 
 #FUNÇÃO LEN:
 
 print(f'Contando quantas chaves o dicionario possui usando len: --{len(leonardo)}--')
-print('------------')
+Linha()
 
 #VERIFICANDO A EXISTÊNCIA DE UMA CHAVE:
 
 if "Nome" in leonardo:
     print(f'VERIFICANDO A EXISTÊNCIA DE UMA CHAVE: {leonardo.get("Nome")}')
     
-print('------------')
-from operator import itemgetter
+Linha()
 
 #PARA OBTER AS CHAVES DE UM DICIONARIO (EM LISTA):
-print('Para obter as CHAVES de um dicionario:')
+
+print('Para obter as CHAVES de um dicionario: leonardo.keys()')
+
 for a in leonardo.keys():
         print(a)
         
-print('------------')
+Linha()
 
 
 #PARA OBTER TODOS OS VALORES DE UM DICIONARIO (EM LISTA):
-print('Para obter os VALORES de um dicionario:')
-
+print('Para obter os VALORES de um dicionario: leonardo.values()')
 
 for a in leonardo.values():
     print(a)
 
-print('------------')
-
+Linha()
 
 
 #PARA OBTER TODOS OS ITENS DE UM DICIONARIO (EM LISTA):
-print('Para obter TODOS OS ITENS de um dicionario:')
+
+print('Para obter TODOS OS ITENS de um dicionario: leonardo.items()')
 
 for a in leonardo.items():
     print(a)
 
-print('------------')
-
 #ORDENANDO ELEMENTOS usando SORTED:
-print('------------')
-print("ORDENANDO ELEMENTOS usando SORTED")
+
+Linha()
+
+print("ORDENANDO ELEMENTOS usando -> sorted(leonardo)")
+
 for i in sorted(leonardo):
     print(i)
 
-print('------------')
+Linha()
 
 print("ORDENANDO ELEMENTOS usando SORTED com valores em ordem crescente")
+
 print("---")
+
 compras={}
 compras["Cliente 1"]=10
 compras["Cliente 2"]=50
@@ -107,34 +136,48 @@ print("print(i)")
 for i in sorted(compras.values(),reverse=True):
     print(i)
     
-print('------------')
+Linha()
+
+
+from operator import itemgetter
 
 print("ORDENANDO ELEMENTOS baseado nos valores: itemgetter()")
-print('------------')
+print('from operator import itemgetter')
+Linha()
+
 print("Compras dos clientes em ordem decrescente:")
+
 for i, a in sorted(compras.items(), key=itemgetter(1), reverse=True):
+
     print(f'{i}--{a}')
 
 
-print('------------')
+Linha()
+
 
 print("ORDENANDO ELEMENTOS usando SORTED 'chave(i)' e valor(a)'")
+
 for i, a in sorted(compras.items()):
+
     print(f'{i} ---- {a}')
 
-print('------------')
+
+Linha()
+
 
 #CLONANDO UM DICIONARIO:
 
 copia= dict(leonardo)
 print(f"clonando o dicionario leonardo, atribuindo a variarel copia: print(copia) -> {copia}")
 
-print('------------')
+
+Linha()
 
 
 #REMOVENDO ELEMENTOS USANDO POP:
 leonardo.pop("Nome")
 leonardo.pop("Telefone")
+
 print(f'removendo elementos usando o comando -> leonardo.pop("Nome") e telefone em outra linha.: {leonardo}')
 
-print('------------')
+Linha()
